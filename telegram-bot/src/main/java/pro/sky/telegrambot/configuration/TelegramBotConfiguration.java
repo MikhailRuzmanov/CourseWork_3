c2e985f2b053a95f6cb7632d203ee97f5b567911
MikhailRuzmanov <mihailruzmanov@mail.ru>
2024-03-16 15:18:00 +0300

    Курсовая работа №3 Готово

--- telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -5,9 +5,15 @@ import com.pengrad.telegrambot.model.DeleteMyCommands;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+
 
 @Configuration
 public class TelegramBotConfiguration {
+
+    @Value("${telegram.bot.name}")
+    private String botName;
 
     @Value("${telegram.bot.token}")
     private String token;
